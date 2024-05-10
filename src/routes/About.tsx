cc659c432fe8ce2400bcf40802e53f0452dbdcfa
Shahar <shahar.efron@gmail.com>
2024-05-10 11:39:02 +0300

    feat: add router

---
+++ src/routes/About.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export interface IProps { }
+
+const About: React.FC<IProps> = () => {
+
+  return (
+    <div className="About">
+        <p>About</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export interface IProps { }
+
+const Home: React.FC<IProps> = () => {
+
+  return (
+    <div className="Home">
+        <p>home</p>
+    </div>
+  );
+};
+
+export default Home;
