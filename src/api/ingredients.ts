5686214909797b1ee67b3cff27989560db17f955
Alin <rsdans5@gmail.com>
2024-08-24 19:20:41 +0300

    added backend url

--- src/api/ingredients.ts
+++ src/api/ingredients.ts
@@ -1,15 +1,12 @@
 import axios from "axios";
+import { backendUrl } from "./config";
 
 export const getSkinTypeByIngredients = async (formData: FormData) => {
-  const response = await axios.post(
-    "http://localhost:8000/ingredients-list",
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
+  const response = await axios.post(backendUrl, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
   return response.data;
 };
