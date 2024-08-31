645464200983959a934b0367d5bdbe059c1ea862
oriyafe1 <ori987yafe@gmail.com>
2024-08-31 12:20:22 +0300

    Add option to open camera

--- src/components/shared/CameraAndUpload.tsx
+++ src/components/shared/CameraAndUpload.tsx
@@ -1,7 +1,13 @@
-import React, { useState, useCallback } from "react";
-import Cropper, { Area } from "react-easy-crop";
-import getCroppedImg from "../../utils/cropImage";
-import "../../styles/components/CameraAndUpload.css";
+import React, { useState, useCallback, useRef } from 'react';
+import Cropper, { Area } from 'react-easy-crop';
+import Webcam from 'react-webcam';
+import getCroppedImg from '../../utils/cropImage';
+import '../../styles/components/CameraAndUpload.css';
+
+const videoConstraints = {
+  width: 540,
+  facingMode: 'user',
+};
 
 export interface ICameraAndUploadProps {
   photoSrc: string | null;
@@ -25,6 +31,8 @@ const CameraAndUpload: React.FC<ICameraAndUploadProps> = ({
   const [crop, setCrop] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
   const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area | null>(null);
+  const [isCameraOpen, setIsCameraOpen] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
 
   const onCropComplete = useCallback(
     (croppedArea: Area, croppedAreaPixels: Area) => {
@@ -40,7 +48,7 @@ const CameraAndUpload: React.FC<ICameraAndUploadProps> = ({
 
       reader.onload = () => {
         const fileContent = reader.result;
-        if (typeof fileContent === "string") {
+        if (typeof fileContent === 'string') {
           setPhotoSrc(fileContent);
           setIsCropping(!isCropping);
         }
@@ -66,7 +74,7 @@ const CameraAndUpload: React.FC<ICameraAndUploadProps> = ({
     try {
       const croppedImage = await getCroppedImg(photoSrc, croppedAreaPixels);
       const formData = new FormData();
-      formData.append("file", croppedImage);
+      formData.append('file', croppedImage);
 
       const response = await imgProccess(formData);
       resultSetter(Array.isArray(response) ? response : [response]);
@@ -76,21 +84,64 @@ const CameraAndUpload: React.FC<ICameraAndUploadProps> = ({
     }
   };
 
+  const openCamera = () => setIsCameraOpen(true);
+
+  const capturePhoto = useCallback(async () => {
+    if (webcamRef.current === null) return;
+
+    const imageSrc = webcamRef.current.getScreenshot();
+    setPhotoSrc(imageSrc);
+    setIsCameraOpen(false);
+    setIsCropping(true);
+  }, [webcamRef, setPhotoSrc, setIsCameraOpen, setIsCropping]);
+
+  const onUserMedia = (e: any) => {
+    console.log(e);
+  };
+
   return (
-    <div className="cameraAndUpload">
-      <button className="generalButton__primary">
-        <label htmlFor="fileInput">Open Camera or Upload Photo</label>
-      </button>
-      <input
-        id="fileInput"
-        type="file"
-        accept="image/*"
-        capture="environment"
-        onChange={handleFileChange}
-        style={{ display: "none" }}
-      />
+    <div className='cameraAndUpload'>
+      {!isCameraOpen && (
+        <>
+          <button
+            onClick={openCamera}
+            className='generalButton__primary'
+            style={{ marginRight: '10px' }}
+          >
+            Open Camera
+          </button>
+          <button className='generalButton__primary'>
+            <label htmlFor='fileInput'>Upload Photo</label>
+          </button>
+          <input
+            id='fileInput'
+            type='file'
+            accept='image/*'
+            capture='environment'
+            onChange={handleFileChange}
+            style={{ display: 'none' }}
+          />
+        </>
+      )}
+      {isCameraOpen && (
+        <>
+          <Webcam
+            ref={webcamRef}
+            audio={false}
+            screenshotFormat='image/jpeg'
+            videoConstraints={videoConstraints}
+            onUserMedia={onUserMedia}
+          />{' '}
+          <br />
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button onClick={capturePhoto} className='generalButton__primary'>
+              Capture Photo
+            </button>
+          </div>
+        </>
+      )}
       {photoSrc && isCropping && (
-        <div className="cropContainer">
+        <div className='cropContainer'>
           <Cropper
             image={photoSrc}
             crop={crop}
@@ -100,16 +151,16 @@ const CameraAndUpload: React.FC<ICameraAndUploadProps> = ({
             onZoomChange={setZoom}
             onCropComplete={onCropComplete}
           />
-          <button className="generalButton__primary save" onClick={handleSave}>
+          <button className='generalButton__primary save' onClick={handleSave}>
             Save
           </button>
         </div>
       )}
       {photoSrc && !isCropping && (
-        <div className="photoContainer">
-          <img className="photo" src={photoSrc} alt="Captured or Uploaded" />
+        <div className='photoContainer'>
+          <img className='photo' src={photoSrc} alt='Captured or Uploaded' />
           <button
-            className="generalButton__primary"
+            className='generalButton__primary'
             onClick={handleCropAndAnalyze}
           >
             Analyze
