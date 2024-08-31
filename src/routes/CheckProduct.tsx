7d3710fbe279728f565a44060db02c9a25d34656
AlinSab <62662556+AlinSab@users.noreply.github.com>
2024-08-31 20:00:05 +0300

    Adjust skin types to ingredients model (#21)

--- src/routes/CheckProduct.tsx
+++ src/routes/CheckProduct.tsx
@@ -2,11 +2,15 @@ import React, { useMemo, useState } from 'react';
 import { CameraAndUpload, Header } from '../components/shared';
 import { useNavigate } from 'react-router-dom';
 import '../styles/components/CheckProduct.css';
-import { SkinConditions, SkinTypes } from '../generalTypes';
+import { ProductSkinConditions, ProductSkinTypes } from '../generalTypes';
 import { getSkinTypeByIngredients } from '../api/ingredients';
 import { getClassificationInfo } from '../util/classification';
 import { useRecoilValue } from 'recoil';
 import { classificationState } from '../atoms/classification.atom';
+import { FaCheck } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+import { faceTypeToProductTypeMapping } from '../util/faceAndProductMapping';
+import { getCommonValues } from '../util/util';
 
 const CheckProduct: React.FC = () => {
   const [photoSrc, setPhotoSrc] = useState<string | null>(null);
@@ -16,6 +20,11 @@ const CheckProduct: React.FC = () => {
     () => getClassificationInfo(classification?.[0])?.label,
     [classification]
   );
+  const classificationValue = useMemo<string | undefined>(
+    () => getClassificationInfo(classification?.[0])?.value,
+    [classification]
+  );
+
   const navigate = useNavigate();
   const [isCropping, setIsCropping] = useState(false);
 
@@ -31,10 +40,38 @@ const CheckProduct: React.FC = () => {
             ? 'oily'
             : currSkinType === 'dryness'
             ? 'dry'
+            : currSkinType === 'dark'
+            ? 'dark circle'
             : currSkinType
         )
       ),
     ]);
+  };
+
+  const getRecommendationMessage = () => {
+    const commonTypes: string[] = getCommonValues(
+      faceTypeToProductTypeMapping[classificationValue!],
+      matchSkinTypes!
+    );
+    let text: string = '';
+    const matchedFaceTypes: string[] = [];
+    const matchedFaceConditions: string[] = [];
+    const skinTypeKeys = Object.keys(ProductSkinTypes) as string[];
+    const skinConditionKeys = Object.keys(ProductSkinConditions) as string[];
+
+    commonTypes.forEach((type) => {
+      if (skinTypeKeys.includes(type)) matchedFaceTypes.push(type);
+      if (skinConditionKeys.includes(type)) matchedFaceConditions.push(type);
+    });
+    if (matchedFaceTypes.length)
+      text = text.concat(matchedFaceTypes.join(', ').concat(' skin, '));
+    if (matchedFaceConditions.length)
+      text = text
+        .concat('skin with '.concat(matchedFaceConditions.join(', ')))
+        .concat(', ');
+
+    return `Based on your skin analysis, this product is suitable for 
+    ${text} which aligns with your skin condition`;
   };
 
   return (
@@ -91,32 +128,65 @@ const CheckProduct: React.FC = () => {
           />
           <span className="generalText generalTitle">Match for skin type:</span>
           <div className="checkProduct__skinTypesList">
-            {Object.keys(SkinTypes).map((type) => (
+            {Object.keys(ProductSkinTypes).map((type) => (
               <div className="checkProduct__skinTypesItem" key={type}>
                 <span className="checkProduct__skinTypesClass">
-                  {matchSkinTypes.includes(type) ? 'V' : 'X'}
+                  {matchSkinTypes.includes(type) ? (
+                    <FaCheck style={{ color: 'green', fontSize: '1em' }} />
+                  ) : (
+                    <AiOutlineClose style={{ color: 'red', fontSize: '1em' }} />
+                  )}
                 </span>
                 <span className="checkProduct__skinTypesText">
-                  {SkinTypes[type as keyof typeof SkinTypes]}
+                  {ProductSkinTypes[type as keyof typeof ProductSkinTypes]}
                 </span>
               </div>
             ))}
           </div>
-          <span className="generalText generalTitle">
-            Match for skin condition
-          </span>
-          <div className="checkProduct__skinTypesList">
-            {Object.keys(SkinConditions).map((type) => (
-              <div className="checkProduct__skinTypesItem" key={type}>
-                <span className="checkProduct__skinTypesClass">
-                  {matchSkinTypes.includes(type) ? 'V' : 'X'}
-                </span>
-                <span className="checkProduct__skinTypesText">
-                  {SkinConditions[type as keyof typeof SkinConditions]}
-                </span>
+          {getCommonValues(Object.keys(ProductSkinConditions), matchSkinTypes)
+            .length ? (
+            <>
+              <span className="generalText generalTitle">
+                Match for skin condition:
+              </span>
+              <div className="checkProduct__skinTypesList">
+                {Object.keys(ProductSkinConditions).map((type) => (
+                  <div className="checkProduct__skinTypesItem" key={type}>
+                    <span className="checkProduct__skinTypesClass">
+                      {matchSkinTypes.includes(type) ? (
+                        <FaCheck style={{ color: 'green', fontSize: '1em' }} />
+                      ) : (
+                        <AiOutlineClose
+                          style={{ color: 'red', fontSize: '1em' }}
+                        />
+                      )}{' '}
+                    </span>
+                    <span className="checkProduct__skinTypesText">
+                      {
+                        ProductSkinConditions[
+                          type as keyof typeof ProductSkinConditions
+                        ]
+                      }
+                    </span>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
+            </>
+          ) : (
+            <></>
+          )}
+          {classificationValue &&
+          matchSkinTypes &&
+          getCommonValues(
+            faceTypeToProductTypeMapping[classificationValue],
+            matchSkinTypes
+          ).length ? (
+            <span className="generalText generalTitle">
+              {getRecommendationMessage()}
+            </span>
+          ) : (
+            <> </>
+          )}
         </div>
       )}
     </div>
