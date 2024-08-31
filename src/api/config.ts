e38bee22edaf4104ad72c372a0c76afed3403dbb
Alin <rsdans5@gmail.com>
2024-08-31 22:01:44 +0300

    update backend url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,2 @@
-// export const backendUrl = "https://skintell-backend-production.up.railway.app";
-export const backendUrl = "http://localhost:8000";
+export const backendUrl = 'https://skintell-backend-production.up.railway.app';
+// export const backendUrl = "http://localhost:8000";
