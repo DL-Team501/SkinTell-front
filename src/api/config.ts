c15a7b78af73648f93e2ec1c1c5d588f15fbe51a
Alin <rsdans5@gmail.com>
2024-09-01 10:10:40 +0300

    change to private route

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,3 @@
-export const backendUrl = 'https://skintell-backend-production.up.railway.app';
+export const backendUrl = 'https://unique-creativity.railway.internal';
+// export const backendUrl = 'https://skintell-backend-production.up.railway.app';
 // export const backendUrl = "http://localhost:8000";
