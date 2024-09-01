d9aacf9312aeddaa4317cbb4dc50b1d84820c87f
Alin <rsdans5@gmail.com>
2024-09-01 10:15:02 +0300

    revert

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,2 @@
-export const backendUrl = 'https://unique-creativity.railway.internal';
-// export const backendUrl = 'https://skintell-backend-production.up.railway.app';
+export const backendUrl = 'https://skintell-backend-production.up.railway.app';
 // export const backendUrl = "http://localhost:8000";
